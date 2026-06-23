e7779c71df299b73b3efc769b4c8756440d4ce76
CorentinAT <corentin.al2004@gmail.com>
2026-06-23 09:20:04 +0200

    references

--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
 CREATE TABLE Utilisateur (
   id_utilisateur INT PRIMARY KEY AUTO_INCREMENT, 
-  nom VARCHAR(50) ,
-  mdp VARCHAR(50) ,
-  promo VARCHAR(5) ,
-  points INT,
-  actif BOOLEAN
+  nom VARCHAR(50),
+  mdp VARCHAR(50),
+  promo VARCHAR(5),
+  points INT DEFAULT 0,
+  actif BOOLEAN DEFAULT 0
 )
 
 CREATE TABLE Post (
-  id_post INT PRIMARY KEY AUTO_INCREMENT , 
+  id_post INT PRIMARY KEY AUTO_INCREMENT, 
   images BLOB,
   description VARCHAR(50),
-  date_post DATE
+  date_post DATE DEFAULT CURRENT_DATE()
 )
 
 CREATE TABLE Vote (
   id_vote INT PRIMARY KEY AUTO_INCREMENT, 
   id_post REFERENCES Post(id),
   id_utilisateur REFERENCES Utilisateur(id),
-  date_vote DATE,
+  date_vote DATE DEFAULT CURRENT_DATE(),
   id_pour_qui_a_vote REFERENCES Utilisateur(id)
 )
 
@@ -26,6 +26,6 @@ CREATE TABLE Commentaire(
   id_com INT PRIMARY KEY AUTO_INCREMENT, 
   id_utilisateur REFERENCES Utilisateur(id),
   id_post REFERENCES Post(id),
-  date_commentaire DATE ,
+  date_commentaire DATE DEFAULT CURRENT_DATE(),
   commentaire VARCHAR(255)
- )
+)
