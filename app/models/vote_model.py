150e86641698621f195e7cefa5039b3a33f946ab
Yasmineboire <yasmine.boire@gmail.com>
2026-06-24 11:39:42 +0200

    delete vote

--- app/models/vote_model.py
+++ app/models/vote_model.py
@@ -12,3 +12,16 @@ def vote(vote, post_id, user_id, voted_user_id):
     finally:
         cur.close()
         conn.close()
+
+def delete_vote(vote_id):
+    conn = get_db()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("DELETE FROM vote WHERE vote_id = ?", (vote_id,))
+        conn.commit()
+    except Exception:
+        raise
+    finally:
+        cur.close()
+        conn.close()
